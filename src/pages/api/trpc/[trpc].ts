67be51f95ab44b471020cc0d0dc2b4c953c9e4e6
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-09-01 19:42:47 +0530

    pushing

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -15,9 +15,14 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 	// Set CORS headers
+	res.setHeader("Access-Control-Request-Method", "*");
+
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 
 	// Handle preflight requests (CORS preflight)
